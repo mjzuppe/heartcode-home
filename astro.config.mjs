2637cca443ae7a6a1a8fe7b4931c8bc2ebb267d7
Mark Zuppe <zuppe@live.com>
2024-03-22 15:58:26 +0000

    fix: Enabled contact form submission via Formspark

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "never",
-  site: 'https://heartandcode.co',
+  site: "https://heartandcode.co",
   integrations: [alpinejs(), tailwind(), sitemap(), icon(), sitemap()],
   output: "static",
 });
