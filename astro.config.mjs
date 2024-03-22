32987ff1636afd5b4664ad95895ad3152bb8ab0a
Mark Zuppe <zuppe@live.com>
2024-03-22 15:48:23 +0000

    feat: :art: Added sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
+import sitemap from '@astrojs/sitemap';
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "never",
-  integrations: [alpinejs(), tailwind(), sitemap(), icon()],
+  site: 'https://heartandcode.co',
+  integrations: [alpinejs(), tailwind(), sitemap(), icon(), sitemap()],
   output: "static",
 });
