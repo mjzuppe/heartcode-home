ae93c5b940c676810999eaf5d54141da9b02ea8e
Mark Zuppe <zuppe@live.com>
2024-03-22 15:43:03 +0000

    feat: :zap: Remove nanostores added alpineJS now SSG enabled

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: "standalone",
-  }),
   trailingSlash: "never",
-  integrations: [tailwind(), sitemap(), icon()],
-  output: "server",
+  integrations: [alpinejs(), tailwind(), sitemap(), icon()],
+  output: "static",
 });
